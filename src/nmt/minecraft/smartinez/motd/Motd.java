6257e4e3487db6f59bfbafe76577f2799308126d
Stephanie Martinez <smarti02@nmt.edu>
2015-02-16 10:41:57 -0700

    got basic motd working. will read text from the config.yml and display it to the user on login

--- src/nmt/minecraft/smartinez/motd/Motd.java
+++ src/nmt/minecraft/smartinez/motd/Motd.java
@@ -1,25 +1,31 @@
 package nmt.minecraft.smartinez.motd;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Motd extends JavaPlugin implements Listener{
-	//private String message = "ADMIN HAS NOT YET SET UP THE MESSAGE OF THE DAY";	
+	private String message = "ADMIN HAS NOT YET SET UP THE MESSAGE OF THE DAY";	
 	
-	void OnEnable(){
-		//getServer().getPluginManager().registerEvents(this, this);
+	@Override
+	public void onEnable(){
+		this.saveDefaultConfig();
+		getServer().getPluginManager().registerEvents(this, this);
 		getLogger().info("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHH TESTING STUFF");
-		System.out.println("MOOOOOORRRRRRRRRRRRRRRRRR STTTTTUUUUUUUUUFFFFFFFFFFF");
 		//get the message and save it
-		
+		message = this.getConfig().get("message").toString();
 	}
-	/*
+	
+	
 	//detect when a player logs in
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void onLogin(PlayerLoginEvent event) {
-		//print the motd to that user's terminal
-		event.getPlayer().sendMessage(message);
-		getLogger().info(event.getPlayer().getDisplayName()+"logged in AND GOT THE MESSAGE");
+	@EventHandler 
+	public void onPlayerJoin(PlayerJoinEvent event){
+		  Player player = event.getPlayer();
+		  event.setJoinMessage("");
+		  getLogger().info("PPPPPPPPPPPPPPPLLLLLLLLLLLLLLLLLAYYYYYYYYYYYYYYYYYEEEEERRRRRRRRRRR");
+		  player.sendMessage(message);
 	}
-		*/
+		
 }
